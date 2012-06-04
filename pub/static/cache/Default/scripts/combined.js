a4c2e9ff585cf44a458631a52661bdfec88c2be0
lnwdr <leon.weidauer@gmail.com>
2012-06-04 14:55:59 +0200

    Added Twig rendering engine, changed default rendeer to Twig

--- pub/static/cache/Default/scripts/combined.js
+++
@@ -1,5 +0,0 @@
-
-
-/* /www/app/modules/Default/resources/scripts/test_log.js */
-
-(function(e){return console.log("module JS resource has been loaded"),e})(1);
--- pub/static/cache/_global/scripts/combined.js
+++
@@ -1,5 +0,0 @@
-
-
-/* /www/app/resources/scripts/test_log.js */
-
-console.log("global JS resource has been loaded");
