7fb24fb083d1c45d412a8cac357772e78bb49fa9
Leon Weidauer <leon.weidauer@googlemail.com>
2013-02-15 15:56:49 +0100

    Working authentication with SimpleAuthProvider and index page

--- app/modules/Common/impl/Header/HeaderSuccessView.class.php
+++ app/modules/Common/impl/Header/HeaderSuccessView.class.php
@@ -5,7 +5,5 @@ class Common_Header_HeaderSuccessView extends CommonBaseView
     public function executeHtml(AgaviRequestDataHolder $parameters)
     {
         $this->setupHtml($parameters);
-
-        $this->setAttribute('modules', $modules);
     }
 }
