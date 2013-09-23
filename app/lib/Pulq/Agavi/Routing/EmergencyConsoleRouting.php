ac5e140a5aabb5da2ce232ffdc7ca9fc32c8d646
Leon Weidauer <leon.weidauer@googlemail.com>
2013-09-23 14:01:25 +0200

    added emergency routing and environment

---
+++ app/lib/Pulq/Agavi/Routing/EmergencyConsoleRouting.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pulq\Agavi\Routing;
+
+use \AgaviConsoleRouting;
+use \AgaviConfig;
+use \AgaviConfigCache;
+use \Exception;
+
+class EmergencyConsoleRouting extends AgaviConsoleRouting
+{
+
+    protected function loadConfig()
+    {
+        $cfg = AgaviConfig::get('core.config_dir') . '/emergency/routing.xml';
+        // allow missing routing.xml when routing is not enabled
+        if(!is_readable($cfg)) {
+            throw new Exception("Emergency routing file not found! ($cfg)");
+        }
+        $this->importRoutes(unserialize(file_get_contents(AgaviConfigCache::checkConfig($cfg, $this->context->getName()))));
+    }
+}
