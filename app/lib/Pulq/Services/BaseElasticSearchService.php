43167d48c7c4f7e7b06cd2f4c77b80eb4aa841aa
Leon Weidauer <leon.weidauer@googlemail.com>
2014-02-14 16:47:20 +0100

    Revert "fixed filters"
    This reverts commit 8d8d934e76b5d28bb1c0bf98086af8d05abb5157.

--- app/lib/Pulq/Services/BaseElasticSearchService.php
+++ app/lib/Pulq/Services/BaseElasticSearchService.php
@@ -2,10 +2,8 @@
 
 namespace Pulq\Services;
 use Pulq\Data\DataObjectSet;
-use Pulq\Exceptions\NotFoundException;
 use Elastica\ResultSet;
 use Elastica\Query;
-use Elastica\Filter;
 
 abstract class BaseElasticSearchService extends BaseService {
     protected $es_index = null;
@@ -17,71 +15,13 @@ abstract class BaseElasticSearchService extends BaseService {
         $this->index = \AgaviContext::getInstance()->getDatabaseManager()->getDatabase($this->es_index)->getResource();
     }
 
-    public function getById($id)
-    {
-        $query = new Query\Field('_id', $id);
-        $resultData = $this->executeQuery(Query::create($query), false);
-
-        $set = $this->extractFromResultSet($resultData);
-
-        if ($set->getTotalCount() < 1)
-        {
-            throw new NotFoundException($this->data_object_class . ' not found.');
-        }
-
-        return $set[0];
-    }
-
-    public function getByIds(array $ids)
-    {
-        if (empty($ids))
-        {
-            return new DataObjectSet(array());
-        }
-
-        $query = new Query\Field('_id', implode(' ', $ids));
-        $resultData = $this->executeQuery(Query::create($query));
-
-        $set = $this->extractFromResultSet($resultData);
-
-        return $set;
-    }
-
-    public function getAll()
-    {
-        $query = new Query\MatchAll();
-
-        $resultData = $this->executeQuery(Query::create($query));
-
-        $set = $this->extractFromResultSet($resultData);
-
-        return $set;
-    }
-
     protected function getType()
     {
         return $this->index->getType($this->es_type);
     }
 
-    protected function executeQuery(Query $query, $use_live_filter = true)
+    protected function executeQuery(Query $query)
     {
-        if ($use_live_filter) {
-            $bool_filter = new Filter\Bool();
-
-            $live_query = new Query\Field('live', "true");
-            $live_filter = new Filter\Query($live_query);
-
-            $bool_filter->addMust($live_filter);
-
-            if ($query->hasParam('filter')) {
-                $existing_filter = $query->getParam('filter');
-                $bool_filter->addMust($existing_filter);
-            }
-
-            $query->setFilter($bool_filter);
-            $query->setSize(100000);
-        }
-
         return $this->getType()->search($query);
     }
 
@@ -91,9 +31,7 @@ abstract class BaseElasticSearchService extends BaseService {
 
         foreach($resultSet->getResults() as $result)
         {
-            $id = $result->getId();
             $data = $result->getData();
-            $data['_id'] = $id;
 
             $class_name = $this->data_object_class;
             $data_objects[] = $class_name::fromArray($data);
