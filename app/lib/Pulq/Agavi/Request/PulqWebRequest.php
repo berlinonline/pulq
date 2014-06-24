6767ad58d4f2ed6603e8e4a7c86566e33e9b7071
Leon Weidauer <leon.weidauer@googlemail.com>
2014-06-24 16:40:44 +0200

    added https-switch via headers

---
+++ app/lib/Pulq/Agavi/Request/PulqWebRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pulq\Agavi\Request;
+
+use \AgaviWebRequest;
+use \AgaviContext;
+
+class PulqWebRequest extends AgaviWebRequest
+{
+    protected $https_headers = array(
+        'HTTPS' => 'yes',
+        'X_SSL' => 'yes',
+    );
+
+    public function initialize(AgaviContext $context, array $parameters = array()) {
+        parent::initialize($context, $parameters);
+
+        $force_https = false;
+
+        foreach($this->https_headers as $header_name => $header_value) {
+            $full_header_name = "HTTP_$header_name";
+
+            if (isset($_SERVER[$full_header_name])) {
+                if ($_SERVER[$full_header_name] === $header_value) {
+                    $force_https = true;
+                }
+            }
+        }
+
+        if ($force_https) {
+            $this->urlPort = 443;
+            $this->urlScheme = "https";
+        }
+    }
+}
