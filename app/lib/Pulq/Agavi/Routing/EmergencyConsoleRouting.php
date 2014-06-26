3f2092ab95ddbc958764b7d83ce4c6068cf7264a
Leon Weidauer <leon.weidauer@googlemail.com>
2014-06-26 12:04:23 +0200

    fixed emergency routing

--- app/lib/Pulq/Agavi/Routing/EmergencyConsoleRouting.php
+++ app/lib/Pulq/Agavi/Routing/EmergencyConsoleRouting.php
@@ -12,7 +12,7 @@ class EmergencyConsoleRouting extends AgaviConsoleRouting
 
     protected function loadConfig()
     {
-        $cfg = AgaviConfig::get('core.config_dir') . '/emergency/routing.xml';
+        $cfg = AgaviConfig::get('core.pulq_dir') . '/app/config/emergency/routing.xml';
         // allow missing routing.xml when routing is not enabled
         if(!is_readable($cfg)) {
             throw new Exception("Emergency routing file not found! ($cfg)");
