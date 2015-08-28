5ff381e3c66f43fdcdd347dbecec161caed51e40
Thorsten Schmitt-Rink <schmittrink@gmail.com>
2015-08-28 14:51:55 +0000

    fixed configuration after agavi is loaded by composer

--- app/config.php
+++ app/config.php
@@ -1,17 +1,10 @@
 <?php
 require(dirname(__DIR__) . '/vendor/autoload.php');
-require(dirname(__DIR__) . '/vendor/honeybee/agavi/src/agavi.php');
 
 AgaviConfig::set('core.app_dir', __DIR__);
 AgaviConfig::set('core.pub_dir', realpath(__DIR__.'/../pub'));
 AgaviConfig::set('core.project_dir', realpath(__DIR__.'/../../project'));
 AgaviConfig::set('core.modules_dir', __DIR__ . DIRECTORY_SEPARATOR . 'modules');
-AgaviConfig::set(
-    'core.agavi_dir', 
-    dirname(__DIR__) . str_replace(
-        '/', DIRECTORY_SEPARATOR, '/vendor/honeybee/agavi/src/'
-    )
-);
 
 // without this, the template_dir setting in settings.xml won't work …
 AgaviConfig::set('core.template_dir', null);
